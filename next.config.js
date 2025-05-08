d7b44a7cf812967c0605f5223c7fd2126e653909
Klim <subbotin.klim@gmail.com>
2025-05-08 14:23:20 +0200

    Update next config

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['telegra.ph', 'api.ksubbotin.ru', 'unsplash.com'],
+    domains: ['api.ksubbotin.ru'],
   },
 };
 
